735a1d221e861ab45cfc06c1cecf79ac55b460ea
tgruza <tgruza@wp.pl>
2021-06-30 10:28:43 +0200

    url formatter update

--- src/main/java/pl/tgruza/webcategorizer/model/WebsiteDto.java
+++ src/main/java/pl/tgruza/webcategorizer/model/WebsiteDto.java
@@ -1,2 +1,15 @@
-package pl.tgruza.webcategorizer.model;public class WebsiteDto {
+package pl.tgruza.webcategorizer.model;
+
+import java.util.Set;
+
+public class WebsiteDto {
+    private Set<Category> categories;
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
+    }
 }
--- src/main/java/pl/tgruza/webcategorizer/repository/CategoryRepository.java
+++ src/main/java/pl/tgruza/webcategorizer/repository/CategoryRepository.java
@@ -7,4 +7,6 @@ import pl.tgruza.webcategorizer.model.Category;
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
     Category findByName(String name);
+
+    boolean existsByName(String name);
 }
