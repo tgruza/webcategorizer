97fb30194e3aa2d4bb3dd32c9e36e2c040e70fa5
Tomasz Gruza <56762122+tgruza@users.noreply.github.com>
2021-06-30 10:22:50 +0200

    Url formatter update

--- src/main/java/pl/tgruza/webcategorizer/controller/CheckUrlController.java
+++ src/main/java/pl/tgruza/webcategorizer/controller/CheckUrlController.java
@@ -1,31 +1,24 @@
 package pl.tgruza.webcategorizer.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.resource.HttpResource;
 import org.springframework.web.servlet.view.RedirectView;
 import pl.tgruza.webcategorizer.exception.UrlNotFoundException;
 import pl.tgruza.webcategorizer.model.Category;
 import pl.tgruza.webcategorizer.model.Website;
+import pl.tgruza.webcategorizer.model.WebsiteDto;
 import pl.tgruza.webcategorizer.service.CategoryService;
 import pl.tgruza.webcategorizer.service.WebsiteService;
 
-import java.io.*;
-import java.net.HttpURLConnection;
+import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
-
-
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.nio.ByteBuffer;
-import java.util.HashSet;
-import java.util.Optional;
-import java.util.Set;
-import java.util.concurrent.Flow;
+import java.util.*;
 
 @Controller
 public class CheckUrlController {
@@ -58,7 +51,6 @@ public class CheckUrlController {
     @PostMapping("/checkWebsite")
     public RedirectView checkWebsiteInDB(@ModelAttribute Website website) throws IOException, URISyntaxException, InterruptedException {
 
-
         website.setUrl(checkUrl(website.getUrl()));
 
         if (website.getUrl().equals("https://")) {
@@ -66,27 +58,32 @@ public class CheckUrlController {
         }
 
         if (!websiteService.existsByUrl(website.getUrl())) {
-
-            Set<Category> categories = sendRequestGetCategories(website.getUrl());
+            List<Category> categories = sendRequestGetCategories(website.getUrl());
 
             if (categories.isEmpty()) {
                 return new RedirectView("/url_problem");
             }
 
-            for (Category category : categories) {
-                categoryService.saveCategory(category);
-                website.setCategories(categories);
+            for (int i = 0; i < categories.size() - 1; i++) {
+                if (!categoryService.categoryExistsByName(categories.get(i).getName())) {
+                    categoryService.saveCategory(categories.get(i));
+                    continue;
+                }
+                categories.set(i, categoryService.getByCategory(categories.get(i).getName()));
             }
 
+            website.setCategories(new HashSet<>(categories));
             websiteService.saveWebsite(website);
 
+            categories.clear();
             return new RedirectView("/checkUrl/" + website.getId());
         }
 
         return new RedirectView("/checkUrl/" + websiteService.getWebsiteByUrl(website.getUrl()).getId());
     }
 
-    private Set<Category> sendRequestGetCategories(String url) throws IOException, URISyntaxException, InterruptedException {
+    //Method sends request to API and returns response to formatter method
+    private List<Category> sendRequestGetCategories(String url) throws IOException, URISyntaxException, InterruptedException {
 
         HttpClient client = HttpClient.newHttpClient();
 
@@ -101,7 +98,70 @@ public class CheckUrlController {
 
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
+        return getCategories(response.body());
+    }
 
+    //Formatter method for JSON.toString parameter. Returns set of categories
+    private List<Category> getCategories(String jsonString) {
+        String[] temp = jsonString.split("\"");
+        List<Category> categories = new ArrayList<>();
+
+        for (int i = 0; i < temp.length; i++) {
+            if (temp[i].equals("name")) {
+                categories.add(new Category(temp[i + 2]
+                        .substring(2)
+                        .replace("\\", "")
+                        , null));
+            }
+        }
+
+        return categories;
+    }
+
+    //Format url to www.example.com version
+    private String checkUrl(String url) {
+        url = url.toLowerCase();
+        String[] s = url.split("/");
+
+        for (String word : s) {
+            if (!word.equals("") && word.contains(".")) {
+                url = word;
+                break;
+            }
+        }
+
+        return "https://" + url;
+    }
+
+    //delete Website from database
+    @DeleteMapping("/checkUrl/delete/{id}")
+    public RedirectView deleteWebsite(@PathVariable Long id) {
+
+        Optional<Website> website = websiteService.getWebsiteById(id);
+        website.ifPresent(value -> websiteService.deleteWebsite(value.getId()));
+
+        return new RedirectView("/allRestaurants/{id}");
+    }
+
+    //updating website category
+    @PatchMapping("/edit_url/{id}")
+    public ResponseEntity<?> partialWebsiteUpdate(
+            @RequestBody WebsiteDto websiteUpdated, @PathVariable("id") Long id) {
+
+        Optional<Website> website = websiteService.getWebsiteById(id);
+
+        if (website.isPresent() && !websiteUpdated.getCategories().isEmpty()) {
+            website.get().setCategories(websiteUpdated.getCategories());
+            websiteService.saveWebsite(website.get());
+        }
+
+        return ResponseEntity.ok("website category updated");
+    }
+
+
+
+//  Request to API using HttpURLConnection
+//
 //        String payload = "{\"url\":\"" + url + "\"}\n";
 //
 //        URL klazify = new URL("https://www.klazify.com/api/categorize");
@@ -130,46 +190,4 @@ public class CheckUrlController {
 //
 //        in.close();
 //        con.disconnect();
-
-        return getCategories(response.body());
-    }
-
-    private Set<Category> getCategories(String jsonString) {
-        String[] temp = jsonString.split("\"");
-        Set<Category> categories = new HashSet<>();
-
-        for (int i = 0; i < temp.length; i++) {
-            if (temp[i].equals("name")) {
-                categories.add(new Category(temp[i + 2]
-                        .substring(2)
-                        .replace("\\", "")
-                        , null));
-            }
-        }
-
-        return categories;
-    }
-
-    private String checkUrl(String url) {
-        url = url.toLowerCase();
-        String[] s = url.split("/");
-
-        for (String word : s) {
-            if (!word.equals("") && word.contains("www")) {
-                url = word;
-                break;
-            }
-        }
-
-        return "https://" + url;
-    }
-
-    //delete Website from database
-    @DeleteMapping("/checkUrl/delete")
-    public RedirectView deleteWebsite(@ModelAttribute Website website) {
-
-        websiteService.deleteWebsite(website.getId());
-
-        return new RedirectView("/allRestaurants/{id}");
-    }
 }
