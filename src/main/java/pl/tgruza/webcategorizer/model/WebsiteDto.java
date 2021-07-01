43aeec30ef889693f4993a50847e484043092713
Tomasz Gruza <56762122+tgruza@users.noreply.github.com>
2021-07-01 09:55:18 +0200

    Update WebsiteDto.java

--- src/main/java/pl/tgruza/webcategorizer/model/WebsiteDto.java
+++ src/main/java/pl/tgruza/webcategorizer/model/WebsiteDto.java
@@ -1,4 +1,3 @@
-
 package pl.tgruza.webcategorizer.model;
 
 import java.util.Set;
@@ -14,20 +13,3 @@ public class WebsiteDto {
         this.categories = categories;
     }
 }
-
-package pl.tgruza.webcategorizer.model;
-
-import java.util.Set;
-
-public class WebsiteDto {
-    private Set<Category> categories;
-
-    public Set<Category> getCategories() {
-        return categories;
-    }
-
-    public void setCategories(Set<Category> categories) {
-        this.categories = categories;
-    }
-}
-
